ed6ba9b1510a0c5cf13e83b5be8378264445f9aa
thisishamza <f168345@nu.edu.pk>
2022-02-21 17:01:46 +0500

    Section:02 Event Binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
   allowNewServer:boolean = false;
+  serverCreationStatus:string ='No server was created.';
+  serverName:string = '';
   constructor() {
     setTimeout(() =>{
       this.allowNewServer = true;
@@ -17,4 +19,10 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created.'
+  }
+  OnUpdateServerName(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value; //explicit casting of input type for typescript
+  }
 }
