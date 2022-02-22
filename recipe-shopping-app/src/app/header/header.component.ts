cae39c2295915eb10fd0ba574b118d67ce6e8ceb
thisishamza <f168345@nu.edu.pk>
2022-02-22 16:35:13 +0500

    Fixed component placement in App

--- recipe-shopping-app/src/app/header/header.component.ts
+++ recipe-shopping-app/src/app/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent{
 
