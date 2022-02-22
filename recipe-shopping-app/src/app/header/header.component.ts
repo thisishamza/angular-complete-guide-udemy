6f57ad9a6c10bcbdff79b288fcb1178573116243
thisishamza <f168345@nu.edu.pk>
2022-02-22 17:25:06 +0500

    Added header html

--- recipe-shopping-app/src/app/header/header.component.ts
+++ recipe-shopping-app/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent{
+  collapsed = true;
 
 }
