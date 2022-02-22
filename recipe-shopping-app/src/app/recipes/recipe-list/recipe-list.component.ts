56d271f7fd7882058a4de50918c839eb535bef4a
thisishamza <f168345@nu.edu.pk>
2022-02-22 17:42:18 +0500

    Added recipe model and template code

--- recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe("Title recipe","test recipe","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg")
+  ];
   constructor() { }
 
   ngOnInit(): void {
