024a040247fd145f5fb400325eabcabcf39526b1
thisishamza <f168345@nu.edu.pk>
2022-03-01 16:58:45 +0500

    Section06: added route using ngif and event emitter

--- recipe-shopping-app/src/app/app.component.ts
+++ recipe-shopping-app/src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'recipe-shopping-app';
+  loadedFeature = 'recipe';
+  onNavigate(feature: string) {
+    this.loadedFeature = feature;
+  }
 }
