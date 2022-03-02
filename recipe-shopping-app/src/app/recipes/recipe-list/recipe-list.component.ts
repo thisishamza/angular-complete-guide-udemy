c3adad2cd35b62ee801e3044f30541933483f373
thisishamza <f168345@nu.edu.pk>
2022-03-02 12:33:54 +0500

    Section:06 Added recipe click events to show recipe details

--- recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe-shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from './recipe.model';
 
 @Component({
@@ -7,13 +7,20 @@ import { Recipe } from './recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe("Title recipe","Recipe for creating the best angular components","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg"),
-    new Recipe("Title recipe","Recipe for creating the best angular components","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg"),
+    new Recipe("Component recipe","Recipe for creating the best angular components","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg"),
+    new Recipe("Module recipe","Recipe for creating the best angular modules","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg"),
+    new Recipe("Model recipe","Recipe for creating the best angular models","https://cdn.pixabay.com/photo/2015/08/25/03/50/herbs-906140_960_720.jpg"),
+
   ];
   constructor() { }
 
   ngOnInit(): void {
+  }
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
