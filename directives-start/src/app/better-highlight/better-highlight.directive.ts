0e33e11f3894bab03c4a82f9ae185635e89f4942
thisishamza <f168345@nu.edu.pk>
2022-03-04 17:41:41 +0500

    Section 07: Added hostlistner and hostbinding decorator usage in custom directives

--- directives-start/src/app/better-highlight/better-highlight.directive.ts
+++ directives-start/src/app/better-highlight/better-highlight.directive.ts
@@ -1,17 +1,34 @@
-import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2} from '@angular/core';
 
 // render is a better approach to access DOM.
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
   }
+
+  @HostBinding('style.BackgroundColor') backgroundColor = 'transparent';
 
   // it's a better practice to initialize in ngOnInit
   ngOnInit() {
     // renderer is a better practice to access DOM elements
-    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'blue');
   }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-
+    this.backgroundColor = 'blue';
+
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    // this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'transparent');
+    this.backgroundColor = 'transparent';
+  }
+
+  @HostListener('click') mouseclick(eventData: Event) {
+    this.renderer.setStyle(this.elRef.nativeElement, 'background-color', 'grey');
+  }
+
 }
