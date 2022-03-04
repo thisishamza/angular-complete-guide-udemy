783cc35444962ceba1eff6d0af2739e15831eda2
thisishamza <f168345@nu.edu.pk>
2022-03-04 16:57:34 +0500

    Section 07: update basic attribute directive

--- directives-start/src/app/basic-highlight/basic-highlight.directive.ts
+++ directives-start/src/app/basic-highlight/basic-highlight.directive.ts
@@ -5,7 +5,6 @@ import {Directive, ElementRef, OnInit} from '@angular/core';
 })
 export class BasicHighlightDirective implements OnInit {
   constructor(private elementRef: ElementRef) {
-    elementRef.nativeElement;
   }
 
   ngOnInit() {
